21ce246aa7f87474389451a0fc43a4677caba6a4
HarshaSalih <harshasalih2000@gmail.com>
2022-12-09 09:55:41 +0530

    commits

--- src/main/java/com/example/libraryapp_backend/controller/LibraryController.java
+++ src/main/java/com/example/libraryapp_backend/controller/LibraryController.java
@@ -2,7 +2,7 @@ package com.example.libraryapp_backend.controller;
 
 import com.example.libraryapp_backend.dao.LibraryDao;
 import com.example.libraryapp_backend.model.Library;
-import com.example.libraryapp_backend.model.UserRegistration;
+//import com.example.libraryapp_backend.model.UserRegistration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
@@ -18,48 +18,48 @@ public class LibraryController {
     @Autowired
     private LibraryDao dao;
 
-    @Autowired
-    private UserRegistration dao1;
+//    @Autowired
+//    private UserRegistration dao1;
 
 
-    @CrossOrigin(origins = "*")
-    @PostMapping(path = "/userlogin",consumes = "application/json",produces = "application/json")
-    public List<UserRegistration>UserLogin(@RequestBody UserRegistration u)
-    {
-//        String userName=String.valueOf(u.getUserName());
-//        String password=String.valueOf(u.getPassword());
-//        System.out.println(userName);
-//        System.out.println(password);
-        //return (List<UserRegistration>) dao1.UserLogin(u.getUserName(),u.getPassword());
-        return "success";
-    }
+//    @CrossOrigin(origins = "*")
+//    @PostMapping(path = "/userlogin",consumes = "application/json",produces = "application/json")
+//    public List<UserRegistration>UserLogin(@RequestBody UserRegistration u)
+//    {
+////        String userName=String.valueOf(u.getUserName());
+////        String password=String.valueOf(u.getPassword());
+////        System.out.println(userName);
+////        System.out.println(password);
+//        //return (List<UserRegistration>) dao1.UserLogin(u.getUserName(),u.getPassword());
+//        return "success";
+//    }
 
-    @CrossOrigin(origins = "*")
-    @PostMapping(path="/userregistration",consumes = "application/json",produces = "application/json")
-    public HashMap<String,String> UserRegistration(@RequestBody UserRegistration r)
-    {
-        System.out.println(r.getName());
-        System.out.println(r.getAadharNo());
-        System.out.println(r.getAddress());
-        System.out.println(r.getPincode());
-        System.out.println(r.getDob());
-        System.out.println(r.getEmail());
-        System.out.println(r.getPhone());
-        System.out.println(r.getUserName());
-        System.out.println(r.getPassword());
-        System.out.println(r.getConfirmPassword());
-        dao1.save(r);
-        HashMap<String,String>map=new HashMap<>();
-        map.put("status","success");
-        return  map;
-    }
-
-    @CrossOrigin(origins = "*")
-    @GetMapping("/adminlogin")
-    public List<UserRegistration>AdminLogin()
-    {
-        return (List<UserRegistration>)dao1.findAll();
-    }
+//    @CrossOrigin(origins = "*")
+//    @PostMapping(path="/userregistration",consumes = "application/json",produces = "application/json")
+//    public HashMap<String,String> UserRegistration(@RequestBody UserRegistration r)
+//    {
+//        System.out.println(r.getName());
+//        System.out.println(r.getAadharNo());
+//        System.out.println(r.getAddress());
+//        System.out.println(r.getPincode());
+//        System.out.println(r.getDob());
+//        System.out.println(r.getEmail());
+//        System.out.println(r.getPhone());
+//        System.out.println(r.getUserName());
+//        System.out.println(r.getPassword());
+//        System.out.println(r.getConfirmPassword());
+//        dao1.save(r);
+//        HashMap<String,String>map=new HashMap<>();
+//        map.put("status","success");
+//        return  map;
+//    }
+//
+//    @CrossOrigin(origins = "*")
+//    @GetMapping("/adminlogin")
+//    public List<UserRegistration>AdminLogin()
+//    {
+//        return (List<UserRegistration>)dao1.findAll();
+//    }
 
     @CrossOrigin(origins = "*")
     @PostMapping(path = "/entry",consumes = "application/json",produces = "application/json")
--- src/main/java/com/example/libraryapp_backend/model/UserRegistration.java
+++ src/main/java/com/example/libraryapp_backend/model/UserRegistration.java
@@ -1,129 +1,129 @@
-package com.example.libraryapp_backend.model;
-
-import jakarta.persistence.*;
-
-@Entity
-@Table(name="UserRegistration")
-
-public class UserRegistration {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-
-    private int id;
-    private String name;
-    private String aadharNo;
-    private String address;
-    private int pincode;
-    private String dob;
-    private String email;
-    private String phone;
-    private String userName;
-    private String password;
-    private String confirmPassword;
-
-    public UserRegistration() {
-    }
-
-    public UserRegistration(int id, String name, String aadharNo, String address, int pincode, String dob, String email, String phone, String userName, String password, String confirmPassword) {
-        this.id = id;
-        this.name = name;
-        this.aadharNo = aadharNo;
-        this.address = address;
-        this.pincode = pincode;
-        this.dob = dob;
-        this.email = email;
-        this.phone = phone;
-        this.userName = userName;
-        this.password = password;
-        this.confirmPassword = confirmPassword;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAadharNo() {
-        return aadharNo;
-    }
-
-    public void setAadharNo(String aadharNo) {
-        this.aadharNo = aadharNo;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public int getPincode() {
-        return pincode;
-    }
-
-    public void setPincode(int pincode) {
-        this.pincode = pincode;
-    }
-
-    public String getDob() {
-        return dob;
-    }
-
-    public void setDob(String dob) {
-        this.dob = dob;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
-    }
-}
+//package com.example.libraryapp_backend.model;
+//
+//import jakarta.persistence.*;
+//
+//@Entity
+//@Table(name="UserRegistration")
+//
+//public class UserRegistration {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//
+//    private int id;
+//    private String name;
+//    private String aadharNo;
+//    private String address;
+//    private int pincode;
+//    private String dob;
+//    private String email;
+//    private String phone;
+//    private String userName;
+//    private String password;
+//    private String confirmPassword;
+//
+//    public UserRegistration() {
+//    }
+//
+//    public UserRegistration(int id, String name, String aadharNo, String address, int pincode, String dob, String email, String phone, String userName, String password, String confirmPassword) {
+//        this.id = id;
+//        this.name = name;
+//        this.aadharNo = aadharNo;
+//        this.address = address;
+//        this.pincode = pincode;
+//        this.dob = dob;
+//        this.email = email;
+//        this.phone = phone;
+//        this.userName = userName;
+//        this.password = password;
+//        this.confirmPassword = confirmPassword;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getAadharNo() {
+//        return aadharNo;
+//    }
+//
+//    public void setAadharNo(String aadharNo) {
+//        this.aadharNo = aadharNo;
+//    }
+//
+//    public String getAddress() {
+//        return address;
+//    }
+//
+//    public void setAddress(String address) {
+//        this.address = address;
+//    }
+//
+//    public int getPincode() {
+//        return pincode;
+//    }
+//
+//    public void setPincode(int pincode) {
+//        this.pincode = pincode;
+//    }
+//
+//    public String getDob() {
+//        return dob;
+//    }
+//
+//    public void setDob(String dob) {
+//        this.dob = dob;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPhone() {
+//        return phone;
+//    }
+//
+//    public void setPhone(String phone) {
+//        this.phone = phone;
+//    }
+//
+//    public String getUserName() {
+//        return userName;
+//    }
+//
+//    public void setUserName(String userName) {
+//        this.userName = userName;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getConfirmPassword() {
+//        return confirmPassword;
+//    }
+//
+//    public void setConfirmPassword(String confirmPassword) {
+//        this.confirmPassword = confirmPassword;
+//    }
+//}
